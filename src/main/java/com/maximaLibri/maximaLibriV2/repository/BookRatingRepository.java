4bdef3cd60b64aff684d88b137f9cac19de1b13c
alexandra.murgoci <alexandra.murgoci@ibm.com>
2019-04-22 12:49:57 +0300

    Initial commit

---
+++ src/main/java/com/maximaLibri/maximaLibriV2/repository/BookRatingRepository.java
@@ -0,0 +1,48 @@
+package com.maximaLibri.maximaLibriV2.repository;
+
+import com.maximaLibri.maximaLibriV2.dto.IBookAndRating;
+import com.maximaLibri.maximaLibriV2.dto.IUserHistoryItem;
+import com.maximaLibri.maximaLibriV2.model.BookRating;
+import com.maximaLibri.maximaLibriV2.model.BookRatingId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookRatingRepository extends JpaRepository<BookRating, BookRatingId> {
+    //List<BookRating> findTop10GroupByIdOrderByAvgBookRating();
+    @Query(value = "SELECT * FROM bx_book_ratings WHERE user_id=?1",
+            nativeQuery = true)
+    List<BookRating> findRatingsByUserId(Long userId);
+
+    @Query(value = "SELECT AVG(book_rating) FROM bx_book_ratings WHERE isbn=?1",
+            nativeQuery=true)
+    Float getBookRating(String isbn);
+
+    @Query(value = "SELECT bx_books.isbn, book_title, book_author, year_of_publication, publisher, image_url_s, image_url_m, image_url_l,\n" +
+            "average\n" +
+            "FROM bx_books,\n" +
+            "(\n" +
+            "\tSELECT isbn, average\n" +
+            "FROM (\n" +
+            "\tSELECT isbn, AVG(book_rating) as average\n" +
+            "FROM public.bx_book_ratings as selection1\n" +
+            "GROUP BY isbn\n" +
+            "ORDER BY COUNT(book_rating) DESC\n" +
+            "LIMIT 20\n" +
+            "\t) as selection2\n" +
+            "\tORDER BY average DESC\n" +
+            "\tLIMIT 11\n" +
+            "\t) as selection3\n" +
+            "WHERE bx_books.isbn = selection3.isbn;",
+          nativeQuery=true)
+        List<IBookAndRating> getTop10();
+
+    @Query(value = "SELECT bx_books.isbn,book_title,book_author,book_rating\n" +
+            "FROM public.bx_book_ratings, public.bx_books\n" +
+            "WHERE bx_book_ratings.isbn = bx_books.isbn AND bx_book_ratings.user_id = ?1;",
+            nativeQuery = true)
+    List<IUserHistoryItem> getUserHistory(Long id);
+}
